f366e10e8f0692ba856d36f2d77194f355827adc
Marcelo Dias Schneider <marcelo.d.schneider@gmail.com>
2021-02-09 12:04:26 +0100

    StartBootstrap dataTables Extensions working

--- js/demo/datatables-demo.js
+++ js/demo/datatables-demo.js
@@ -1,6 +1,96 @@
 // Call the dataTables jQuery plugin
 $(document).ready(function() {
-  $('#dataTable').DataTable({
-    "dom" : '<>'
-  });
+  $.getJSON("js/demo/dataTables.json",function(json){
+    $('#dataTable').DataTable({
+      data: json,
+      columns: [
+          { data: 'name' },
+          { data: 'position' },
+          { data: 'office' },
+          { data: 'age' },
+          { data: 'start date'},
+          { data: 'salary' },
+          { data: 'email' }
+      ],
+      "dom": 'QBrlftipPR',
+      searching: true,
+      ordering:  true,
+      paging: true,
+      processing: true,
+      lengthChange: true,
+      info: true,
+      scrollX: false,
+      scrollY: '',
+      scrollCollapse: false,
+      colReorder: true,
+      select: true,
+      fixedHeader: {
+        header: true,
+        footer: false
+      },
+      keys: {
+        columns: ':not(:first-child)',
+        className: 'text-warning bg-secondary',
+        clipboard: true,
+      },
+      fixedColumns: {
+        leftColumns: 1
+      },
+      searchPanes: {
+        cascadePanes: true,
+        // viewTotal: true,
+        layout: 'columns-3',
+      },
+      columnDefs: [
+        {
+            searchPanes: {
+                show: true
+            },
+            targets: [1, 2, 3, 4, 5]
+        }
+      ],
+      buttons: [
+        {
+          extend: 'collection',
+          text: 'Hidden',
+          buttons: [
+            {
+              text: 'Position',
+              action: function ( e, dt, node, config ) {
+                dt.column( -6 ).visible( ! dt.column( -6 ).visible() );
+              }
+            },{
+              text: 'Office',
+              action: function ( e, dt, node, config ) {
+                dt.column( -5 ).visible( ! dt.column( -5 ).visible() );
+              }
+            },{
+              text: 'Age',
+              action: function ( e, dt, node, config ) {
+                dt.column( -4 ).visible( ! dt.column( -4 ).visible() );
+              }
+            },{
+              text: 'Start date',
+              action: function ( e, dt, node, config ) {
+                dt.column( -3 ).visible( ! dt.column( -3 ).visible() );
+              }
+            },{
+              text: 'Salary',
+              action: function ( e, dt, node, config ) {
+                dt.column( -2 ).visible( ! dt.column( -2 ).visible() );
+              }
+            },{
+              text: 'E-mail',
+              action: function ( e, dt, node, config ) {
+                dt.column( -1 ).visible( ! dt.column( -1 ).visible() );
+              }
+            },
+          ]
+        },
+        'copy',
+        'print',
+        'csv',
+      ],
+    })
+  })
 });
